cfa07713dd672105540ddbd001021c10c84b8891
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 09:44:26 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/process_info_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch.hpp>
-
-#include <pqrs/osx/process_info.hpp>
-
-TEST_CASE("globally_unique_string") {
-  auto s1 = pqrs::osx::process_info::globally_unique_string();
-  auto s2 = pqrs::osx::process_info::globally_unique_string();
-  REQUIRE(s1 != s2);
-}
--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,2 +1,15 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <boost/ut.hpp>
+#include <pqrs/osx/process_info.hpp>
+
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  "globally_unique_string"_test = [] {
+    auto s1 = pqrs::osx::process_info::globally_unique_string();
+    auto s2 = pqrs::osx::process_info::globally_unique_string();
+    expect(s1 != s2);
+  };
+
+  return 0;
+}
