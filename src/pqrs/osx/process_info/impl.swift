41ec580166176aab1549e9af39fd095973c8a85c
Takayama Fumihiko <tekezo@pqrs.org>
2023-08-12 06:34:51 +0900

    Update URL

--- src/pqrs/osx/process_info/impl.swift
+++ src/pqrs/osx/process_info/impl.swift
@@ -1,6 +1,6 @@
 // (C) Copyright Takayama Fumihiko 2023.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 import AppKit
 
