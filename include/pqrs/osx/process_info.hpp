1d0386902c489ae062f48ed465f7f96039f4b5f8
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-22 07:55:41 +0900

    v2.4.0

--- include/pqrs/osx/process_info.hpp
+++ include/pqrs/osx/process_info.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::process_info v2.3
+// pqrs::osx::process_info v2.4.0
 
 // (C) Copyright Takayama Fumihiko 2020.
 // Distributed under the Boost Software License, Version 1.0.
